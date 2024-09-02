ff8b5fca9b39f7e64932f0ef692fc5ade2f1ed39
Meetkumar Patel <meetkumar.patel@aircanada.ca>
2024-09-02 13:26:21 -0400

    text change

--- src/Components/CreateNote.js
+++ src/Components/CreateNote.js
@@ -25,7 +25,7 @@ const CreateNote = () => {
   return (
     <div className="form-container">
       <form className="create-note-form" onSubmit={handleSubmit}>
-        <h1>Add Log</h1>
+        <h1>Activity Log</h1>
         <input
           type="text"
           value={noteId}
